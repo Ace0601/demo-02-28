9018f9485358f8cf32dc269f31957700ac89f708
Eloisa Celaje <elocelaje@my.cspc.edu.ph>
2023-02-28 14:36:27 +0800

    Update person.blade.php

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
@@ -16,20 +15,22 @@
     <p style="font-size:12px;">This is the 'person' view, a sample view created on Feb 28, 2023.</p>
 
     <hr>
-        <p><b>Name:</b> {{ $name }}<br>
-        <b>Age:</b> {{ $age }} </p>
-        <p><b>Status:</b></p> 
-        @if($age < 18)
-            <p style="color: red;">You can't vote</p>
-        @else
-            <p style="color: green;">You can vote!</p>
-        @endif 
+        <p>
+        <b>Name:</b> {{ $name }}<br>
+        <b>Age:</b> {{ $age }} 
+        <b>Status:</b> 
+            @if($age < 18)
+                <p style="color: red;">You can't vote</p>
+            @else
+                <p style="color: green;">You can vote!</p>
+            @endif 
+        </p>
     <hr>
 
     <h3>Siblings</h3>
-    <table>
+    <table class="table table-hover table-bordered">
         <thead>
-            <tr>
+            <tr class="table-dark">
                 <th>#</th>
                 <th>Name</th>
                 <th>Age</th>
@@ -45,11 +46,11 @@
                 <td>{{$sibling['name']}}</td>
                 <td>{{$sibling['age']}}</td>
                 <td>
-                @if($sibling['age'] < 18)
-                    <p style="color: red;">You can't vote</p>
-                @else
-                    <p style="color: green;">You can vote!</p>
-                @endif 
+                    @if($sibling['age'] < 18)
+                        <p style="color: red;">You can't vote</p>
+                    @else
+                        <p style="color: green;">You can vote!</p>
+                    @endif 
                 </td>
         </tr>
         @endforeach
